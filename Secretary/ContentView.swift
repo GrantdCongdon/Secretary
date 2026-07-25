f02da2edd94ab82d1c6b5c638c5f7d4b54878582
Grant Congdon <grantdcongdon@gmail.com>
2026-07-25 00:45:25 -0400

    Initial Commit

---
+++ Secretary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Secretary
+//
+//  Created by Grant Congdon on 7/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Secretary/SecretaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretaryApp.swift
+//  Secretary
+//
+//  Created by Grant Congdon on 7/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SecretaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
